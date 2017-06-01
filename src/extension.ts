8592bef35d088ef25b31dc2abd3ee794154b97f7
bagofmice <bagofmice@gmail.com>
2017-06-01 17:22:10 -0400

    Launching mongo shell with parameters and attachng it to terminal instance.

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,9 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+
+var win32 = process.platform === 'win32';
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -10,6 +13,29 @@ export function activate(context: vscode.ExtensionContext) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "mongogo" is now active!');
+    var cp = require('child_process');
+    var path = require('path');
+    var os = require('os');
+    var fs = require('fs');
+
+    var tmpPath: string = os.tmpdir();
+
+    var mongoLocation: string = "C:\\MongoDB\\Server\\3.2\\bin\\mongo.exe";
+
+    var args: string = "";
+    var mongoLaunchString: string = mongoLocation + " " + args;
+    var launchRoot: string = tmpPath + "\\LaunchMongo.bat";
+
+    fs.writeFileSync(launchRoot, mongoLaunchString, 'utf8');
+
+        //vscode.workspace.rootPath
+    
+    
+    console.log(mongoLaunchString);
+    var terminal = vscode.window.createTerminal("Mongo Shell", launchRoot, null )
+    terminal.show();
+        //var writer = cp.spawn
+    var foo = 1;
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
